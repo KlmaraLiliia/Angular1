ad194bb35dea449acb71e569b269895251948369
KlmaraLiliia <khmaraliliia@gmail.com>
2021-06-10 18:42:35 +0300

    Angular1 without services

---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {User} from "../../models/User";
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+users: User[]=[];
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.httpClient.get<User[]>('https://jsonplaceholder.typicode.com/users').subscribe(value => {
+      this.users = value;
+      console.log(value);
+
+    });
+  }
+}
